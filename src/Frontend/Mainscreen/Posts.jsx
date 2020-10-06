cf0cbd8f8e44b1334ad13c46b34f4d51bcbbe89b
Mathias <mathias.skallerud@gmail.com>
2020-10-06 20:14:31 +0200

    Show new subevent in frontend

---
+++ src/Frontend/Mainscreen/Posts.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Posts = ({ post, lang }) => {
+
+    const showUrl = post.googleMaps && post.googleMaps.startsWith('https');
+    const google_maps = post.googleMaps;
+
+    return (
+        <div className="sub-event">
+            <div className="sub-event-time-and-title">
+                <p className="sub-event-time"> rrrrrrr </p>
+                <p className="sub-event-title"> { post.title } </p>
+            </div>
+            {  showUrl && <a className="sub-event-address" href={google_maps} rel="noopener noreferrer" target="_blank"> { lang === 'NO' ? '(kart)' : '(map)' } </a> }
+            
+        </div>
+    )
+
+}
+
+export default Posts;
